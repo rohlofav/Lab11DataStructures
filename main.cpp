19e4b507991b7fff6990f33d628086e016e812a3
Anthony Rohloff <124085634+rohlofav@users.noreply.github.com>
2023-11-30 13:19:13 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,21 +1,21 @@
-#include "PokemonFactory.h"
+// #include "PokemonFactory.h"
 #include <iostream>
 
 int main()
 {
-    std::unique_ptr<Pokemon> myPokemon = PokemonFactory::CreatePokemon(100, 120, 80, "HighAttack", "Water");
+    // std::unique_ptr<Pokemon> myPokemon = PokemonFactory::CreatePokemon(100, 120, 80, "HighAttack", "Water");
 
-    std::cout << "Role: " << myPokemon->GetRole() << std::endl;
-    std::cout << "Type: " << myPokemon->GetType() << std::endl;
-    std::cout << "HP: " << myPokemon->GetHP() << std::endl;
-    std::cout << "Attack: " << myPokemon->GetAttack() << std::endl;
-    std::cout << "Defense: " << myPokemon->GetDefense() << std::endl;
-
-    return 0;
+    // std::cout << "Role: " << myPokemon->GetRole() << std::endl;
+    // std::cout << "Type: " << myPokemon->GetType() << std::endl;
+    // std::cout << "HP: " << myPokemon->GetHP() << std::endl;
+    // std::cout << "Attack: " << myPokemon->GetAttack() << std::endl;
+    // std::cout << "Defense: " << myPokemon->GetDefense() << std::endl;
 
     std::cout << "Lab 11 change here" << std::endl;
 
     std::cout << "More changes" << std::endl;
 
     std::cout << "Even more changes" << std::endl;
+
+    return 0;
 }
